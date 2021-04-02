0b42188a3e6e62e378de9e12fb82fbbe8052135c
hirenbhal <iamhiren5@gmail.com>
2021-04-02 13:57:25 +0530

    set jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const expressJwt = require("express-jwt");
 const User = require("../models/user");
 
 exports.signup = async (req, res) => {
@@ -50,3 +51,8 @@ exports.signout = (req, res) => {
   res.clearCookie("t");
   return res.json({ message: "Signout Success!" });
 };
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+});
--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { getPosts, createPosts } = require("../controllers/post");
+const { requireSignin } = require("../controllers/auth");
 const {createPostValidator} = require("../validator");
 //index.js file automatically reads
 
 const router = express.Router();
 
-router.get("/", getPosts);
+router.get("/",requireSignin, getPosts); 
 router.post("/post", createPostValidator, createPosts);
 
 module.exports = router;
