d68b019ee0e9c08f4c418a17015bb44ad8f46290
hirenbhal <iamhiren5@gmail.com>
2021-04-07 23:52:32 +0530

    tba delete post

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { getPosts, createPosts, postsByUser } = require("../controllers/post");
-const { userById } = require("../controllers/user");
+const { getPosts, createPosts, postsByUser,postById,isPoster,deletePost  } = require("../controllers/post");
+const { userById} = require("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
 const { createPostValidator } = require("../validator");
 //index.js file automatically reads
@@ -15,8 +15,11 @@ router.post(
   createPostValidator
 );
 router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.delete("/post/:postId",requireSignin,isPoster,deletePost);
 
 //any route containing :userId app will first execute userById() method
 router.param("userId", userById);
+//any route containing :postId app will first execute postById() method
+router.param("postId", postById);
 
 module.exports = router;
