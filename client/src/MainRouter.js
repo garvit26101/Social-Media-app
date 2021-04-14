f8e43304adf4a8ef208d6e49eaf6c06a4fda225e
hirenbhal <iamhiren5@gmail.com>
2021-04-14 12:58:52 +0530

    navbar

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+import Navbar from "./core/Navbar";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 
 const MainRouter = () => {
   return (
     <div>
+    <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={Signup} />
